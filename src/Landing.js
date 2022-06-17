983f98dc9ba2cc11bc3af22e55b7cb496b5e6810
Kalyan <kalyanb2000@gmail.com>
2022-06-17 15:29:56 +0530

    cleaned

--- src/Landing.js
+++ src/Landing.js
@@ -1,25 +1,26 @@
-import React from 'react'
+import React,{useState} from 'react'
+// import {Form,InputGroup, Button} from 'react-bootstrap'
 
 function Landing() {
-
+    const [studCount,setstudCount] = useState();
+	const [studRoomCount,setstudRoomCount] = useState();
+	const [sessionCount,setsessionCount] = useState();
+	const [totDuties,settotDuties] = useState(0);
     function main(e)
     {
         e.preventDefault();
-        let studCount = parseInt(document.getElementById("studCount").value);
-        let studRoomCount = parseInt(document.getElementById("studRoomCount").value);
-        let sessionCount = parseInt(document.getElementById("sessionCount").value);
-
-        let totDuties = Math.ceil(studCount/studRoomCount) * 2;
+        settotDuties(Math.ceil(studCount/studRoomCount) * 2);
         console.log(studCount,studRoomCount,totDuties);
 
     }
 
   return (
     <div>
-        <input type="text" id="studCount" placeholder="Student count" />
-        <input type="text" id="studRoomCount" placeholder="Students/Room count" />
-        <input type="text" id="sessionCount" placeholder="Session count" />
+        <input type="text" id="studCount" placeholder="Student count" value={studCount} onChange={e => setstudCount(e.target.value)}/>
+        <input type="text" id="studRoomCount" placeholder="Students/Room count" value={studRoomCount} onChange={e => setstudRoomCount(e.target.value)}/>
+        <input type="text" id="sessionCount" placeholder="Session count" value={sessionCount} onChange={e => setsessionCount(e.target.value)}/>
         <button onClick={main}>submit</button>
+		{totDuties}
     </div>
   )
 }
