196e4479af0253e5243e7a4a53cb05e68f10e8d2
Blazeleo <satviksams@gmail.com>
2022-06-17 15:56:52 +0530

    daylist1.0

--- src/Landing.js
+++ src/Landing.js
@@ -1,28 +1,37 @@
 import React,{useState} from 'react'
-// import {Form,InputGroup, Button} from 'react-bootstrap'
+import coe from './coe.json';
 
 function Landing() {
-    const [studCount,setstudCount] = useState();
-	const [studRoomCount,setstudRoomCount] = useState();
+    const [studCount,setstudCount] = useState(1200);
+	const [studRoomCount,setstudRoomCount] = useState(32);
 	const [sessionCount,setsessionCount] = useState();
 	const [totDuties,settotDuties] = useState(0);
+    
+    const [dayList,setDayList] = useState([]);
+    
+    let doneList = [];
     function main(e)
     {
         e.preventDefault();
         settotDuties(Math.ceil(studCount/studRoomCount) * 2);
         console.log(studCount,studRoomCount,totDuties);
-
+        let tempList = [];
+        coe.map((x) => {
+            tempList.push(x);
+        });   
+        setDayList(tempList);
+        console.log(dayList);
     }
 
   return (
     <div>
-        <input type="text" id="studCount" placeholder="Student count" value={studCount} onChange={e => setstudCount(e.target.value)}/>
+        <input type="text" id="studCount" placeholder="Student count" value={studCount} onChange={e => setstudCount(e.target.value)} />
         <input type="text" id="studRoomCount" placeholder="Students/Room count" value={studRoomCount} onChange={e => setstudRoomCount(e.target.value)}/>
         <input type="text" id="sessionCount" placeholder="Session count" value={sessionCount} onChange={e => setsessionCount(e.target.value)}/>
         <button onClick={main}>submit</button>
-		{totDuties}
+        {dayList.map((x) => {<span>{x}l</span>})}
     </div>
   )
 }
 
-export default Landing
+export default Landing;
