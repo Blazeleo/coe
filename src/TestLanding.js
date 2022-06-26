dbef0aac364ba566152f028ac99d5abcbf9089c0
Kalyan <kalyanb2000@gmail.com>
2022-06-26 19:39:27 +0530

    almost done

--- src/TestLanding.js
+++ src/TestLanding.js
@@ -1,14 +1,20 @@
-import React,{useState,useEffect} from 'react'
-import coe from './coe.json';
+import React, { useState, useEffect } from "react";
+import coe from "./coe.json";
+import Table from 'react-bootstrap/Table'
 
 export default function TestLanding() {
-    const date = [21,22,23,24,25,26];
-    var studCount = 1200,studRoomCount=32,sessionCount,totDuties,assistCount=0,assocCount=0,profCount=0;
+    const date = [21, 22, 23, 24, 25, 26];
+    var studCount = 1200,
+        studRoomCount = 32,
+        sessionCount,
+        totDuties,
+        assistCount = 0,
+        assocCount = 0,
+        profCount = 0;
     var dayList = coe;
-    var doneList=[];
-    for(let i=0; i<date.length;i++)
-        doneList.push([]);
-    const [worked,setWorked] = useState(["worked","2","3"]);
+    var doneList = [];
+    for (let i = 0; i < date.length; i++) doneList.push([[], []]);
+    const [worked, setWorked] = useState([]);
 
     function eachDay(e) {
         e.preventDefault();
@@ -16,78 +22,84 @@ export default function TestLanding() {
             v2 = 0,
             v3 = 0;
         for (let x of dayList) {
-            if (x.desig === "Associate Professor") v2++; // number of Associate Professors
-
-            else if (x.desig === "Professor") v3++; //number of Professors
-
+            if (x.desig === "Associate Professor") v2++;
+            // number of Associate Professors
+            else if (x.desig === "Professor") v3++;
+            //number of Professors
             else v1++; // number of Assistant professors
         }
 
-        totDuties = (studCount / studRoomCount * 12); //duties for duration of exam
-        assistCount = (Math.ceil(0.6 * totDuties) / v1); //number of duties per assistant professor
-        assocCount = (Math.ceil(0.3 * totDuties) / v2); //number of duties per Associate Professor
-        profCount = (Math.ceil(0.1 * totDuties) / v3); //number of duties per Professor
+        totDuties = (studCount / studRoomCount) * 12; //duties for duration of exam
+        assistCount = Math.ceil(0.6 * totDuties) / v1; //number of duties per assistant professor
+        assocCount = Math.ceil(0.3 * totDuties) / v2; //number of duties per Associate Professor
+        profCount = Math.ceil(0.1 * totDuties) / v3; //number of duties per Professor
 
-        date.forEach(daySet);  
-        console.log(doneList) 
-        setWorked(doneList);               
+        date.forEach(daySet);
+
+
+
+
+        console.log(doneList);
+
+        setWorked(doneList);
     }
-    
-    function daySet(value,index,array) {
+
+    function daySet(value, index, array) {
         let seshCount = 1;
         let mornCount = 0;
         let aftCount = 0;
-        let mornLim = totDuties/12;
-        let dayLim = totDuties/6 + 1;
-        
-        for(let x in dayList){
-            if(seshCount === dayLim)
-                break;
+        let mornLim = totDuties / 12;
+        let dayLim = totDuties / 6 + 1;
+
+        for (let x in dayList) {
+            if (seshCount === dayLim) break;
             let lim = 0;
-            if(dayList[x].desig === "Assistant Professor"){
+            if (dayList[x].desig === "Assistant Professor") {
                 lim = assistCount;
-            }
-
-            else if(dayList[x].desig === "Associate Professor"){
+            } else if (dayList[x].desig === "Associate Professor") {
                 lim = assocCount;
-            }
-
-            else{
+            } else {
                 lim = profCount;
             }
 
-            if((dayList[x].total_count < lim) && (dayList[x].day_session_count < 1)){
+            if (dayList[x].total_count < lim && dayList[x].day_session_count < 1) {
                 dayList[x].day_session_count++;
                 dayList[x].total_count++;
-                
-                if((dayList[x].morn === 0) && (seshCount <= mornLim)){
+
+                if (seshCount <= mornLim) {
                     dayList[x].aft = 0;
                     dayList[x].morn = 1;
                     mornCount++;
-                }
-                else{
+                    let mornObj = {
+                        ...dayList[x]
+                    }
+                    doneList[index][0].push(mornObj);
+                } else if(seshCount < dayLim+1) {
                     dayList[x].morn = 0;
                     dayList[x].aft = 1;
                     aftCount++;
+                    let aftObj = {
+                        ...dayList[x]
+                    }
+                    doneList[index][1].push(aftObj);
                 }
-                
+
                 seshCount++;
-                doneList[index].push(dayList[x]);  
 
                 let tempArr = dayList;
                 let elem = tempArr.shift();
                 tempArr.push(elem);
-                dayList = tempArr;             
+                dayList = tempArr;
             }
         }
-        for(let x of dayList){
+        for (let x of dayList) {
             x.day_session_count = 0;
         }
     }
 
     useEffect(() => {
         // console.log(doneList)
-        console.log(worked)
+        console.log(worked);
         // setRemaining(dayList);
     }, [worked]);
 
@@ -98,21 +110,21 @@ export default function TestLanding() {
                 id="studCount"
                 placeholder="Student count"
                 value={studCount}
-                onChange={(e) =>studCount= e.target.value}
+                onChange={(e) => (studCount = e.target.value)}
             />
             <input
                 type="text"
                 id="studRoomCount"
                 placeholder="Students/Room count"
                 value={studRoomCount}
-                onChange={(e) => studRoomCount = (e.target.value)}
+                onChange={(e) => (studRoomCount = e.target.value)}
             />
             <input
                 type="text"
                 id="sessionCount"
                 placeholder="Session count"
                 value={sessionCount}
-                onChange={(e) => sessionCount =(e.target.value)}
+                onChange={(e) => (sessionCount = e.target.value)}
             />
             <button onClick={eachDay}>submit</button>
             {/* {dayList.map((day,index) => (
@@ -126,16 +138,37 @@ export default function TestLanding() {
 				<div>{day.dept}</div>				
 			</div>
 		))} */}
-            <table>
+
+            <Table striped bordered hover>
+                <thead>
+                    <tr>
+                        <td>#</td>
+                        <td colSpan={Math.ceil(studCount / studRoomCount)}>morning</td>
+                        <td colSpan={Math.ceil(studCount / studRoomCount)}>afternoon</td>
+                    </tr>
+                </thead>
                 <tbody>
-                    {date.map((day,index) => (
-                        <tr>
-                            <td>{day}</td>
-                            <td></td>
+                    {worked.map((day, index) => (
+                        <tr key={index}>
+                            <td>{date[index]}</td>
+                            {day.map((individual, index) => (
+                                <>
+                                {individual.map((indi, index) => (<td key={index} className="data-struct">
+                                        <div className="structure">
+                                            <div>{indi.name}</div>
+                                            <div className="small">{indi.desig}</div>
+                                            <hr />
+                                            <div className="small">morning:{indi.morn}</div>
+                                        </div>
+                                    </td>))}
+                                    </>
+                            ))}
                         </tr>
                     ))}
                 </tbody>
-            </table>
+            </Table>
+
+
         </div>
-  )
+    );
 }
